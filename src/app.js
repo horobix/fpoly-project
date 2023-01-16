5343c31c7ff9f35a2b4113801a3df36be94ecdac
huutri <trith13102@gmail.com>
2023-01-17 00:44:47 +0700

    Update something

--- src/app.js
+++ src/app.js
@@ -1,24 +1,25 @@
-// Libraries
+// Libraries importing
 import express, { json } from "express";
 import * as dotenv from "dotenv";
 import ejs from "ejs";
 import path from "path";
 
-// Routes
+// Routes importing
 import indexRouter from "./routes";
 
+// App initialization
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
-// Configs
+// App configuration
 dotenv.config();
-
 app.use(json());
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "views"));
+app.use(express.static(path.resolve(__dirname, "public")));
 
-// Routers
+// Routes applying
 app.use("/", indexRouter);
 
 app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,30 +1,8 @@
 import { Router } from "express";
-import User from "../models/User";
-
 const indexRouter = Router();
 
 indexRouter.get("/", async (req, res) => {
-  let user = await User.findAll({
-    where: {
-      ten: "Thuy",
-    },
-  });
-
-  res.json(`Find: ${JSON.stringify(user)}`);
-});
-
-indexRouter.get("/add", async (req, res) => {
-  let newUser = {
-    email: "thuynnt@fpt.edu.vn",
-    password: "123456",
-    ho: "Nguyen",
-    ten: "Thuy",
-    role: 12,
-    block: true,
-  };
-
-  let insertedUser = await User.create(newUser);
-  console.log(insertedUser);
+  res.render("index");
 });
 
 export default indexRouter;
