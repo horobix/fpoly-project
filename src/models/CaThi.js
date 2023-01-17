290e60b16b8d9e3d440cce917537bfd2a919ac59
Manh Vu <vtmanh106188@gmail.com>
2023-01-17 20:50:17 +0700

    upload

---
+++ src/models/CaThi.js
@@ -0,0 +1,25 @@
+import { Sequelize, DataTypes } from "sequelize";
+import sequelize from "./connect";
+
+const CaThi = sequelize.define("cathis", {
+  tenCaThi: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  thuTu: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  thoiDiem: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  ghiChu: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  
+});
+CaThi.sync({ alter: true });
+
+export default CaThi;
