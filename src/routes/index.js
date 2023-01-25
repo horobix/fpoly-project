aab6cb04cdb80b4da589cbd316000442bef2e066
HoMinhQuang <45233479+drakendark999@users.noreply.github.com>
2023-01-25 15:14:20 +0700

    fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 
-import {getNewGroup} from'../models/CaThi'
+
 
 const indexRouter = Router();
 
 indexRouter.get("/", async ( req,res) => {
-  getNewGroup(req,res)
-  // res.render("index");
+  
+  res.render("index");
 });
 
 export default indexRouter;
