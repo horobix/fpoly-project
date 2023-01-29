a13b6f54e2514261675fe8edd3a27daabd1c448f
nghienhao <91198089+nghienhao@users.noreply.github.com>
2023-01-29 13:19:51 +0700

    Fix code Upload

--- src/models/query/query-add/addUser.js
+++ src/models/query/query-add/addUser.js
@@ -1,54 +1,57 @@
-import * as bcrypt from "bcryptjs";
+import uploadDataUsers from "../query-uploadFile/uploadDataUser";
 import { IncomingForm } from "formidable";
-import * as fs from "fs";
-import path from "path";
 
-const addUser = async (table, req, res, url) => {
-    let id = await table.max("id");
-
+const addUser = async (table, req, res, url, id) => {
     const form = new IncomingForm();
     form.parse(req, function (err, fields, files) {
-        let data = {
-            id: id + 1,
-            ho: fields.ho,
-            ten: fields.ten,
-            email: fields.email,
-            password: fields.password,
-            hinh: files.hinh.originalFilename
-        }
-        let oldPath = files.hinh.filepath;
-        let newPath = path.join(__dirname, `../../../public/img/${data.hinh}`);
-        var rawData = fs.readFileSync(oldPath);
 
-        table
-            .findOne({
-                where: {
-                    email: data.email,
-                },
-            })
-            .then((user) => {
-                if (!user) {
-                    data.password = bcrypt.hashSync(data.password, 10);
-                    console.log(newPath);
-                    console.log(data);
-                    table
-                        .create(data)
-                        .then(() => {
-                            fs.writeFile(newPath, rawData, (err) => (err) ? console.log(err) : '')
-                            res.redirect(url);
+        if (id) {
+            table
+                .findOne({
+                    where: {
+                        id: id,
+                    },
+                })
+                .then(async (user) => {
+                    if (user) {
+                        let data = uploadDataUsers(fields, files);
+
+                        await table.update(data, {
+                            where: {
+                                id: id,
+                            },
                         })
-                        .catch((err) => {
-                            console.log(err);
-                        });
+                        res.redirect(url);
 
-                } else {
-                    alert('Email này đã được đăng ký');
-                    window.history.go(-1);
-                }
-            })
-            .catch((err) => {
-                res.send("error: " + err);
-            });
+                    }
+
+                })
+                .catch((err) => {
+                    res.send("error: " + err);
+                });
+        } else {
+            table
+                .findOne({
+                    where: {
+                        email: fields.email,
+                    },
+                })
+                .then(async (user) => {
+                    if (!user) {
+                        let data = uploadDataUsers(fields, files);
+                        await table.create(data);
+                        res.redirect(url);
+                    } else {
+                        alert('Email này đã được đăng ký');
+                        window.history.go(-1);
+                    }
+                })
+                .catch((err) => {
+                    res.send("error: " + err);
+                });
+        }
+
+
 
     });
 
